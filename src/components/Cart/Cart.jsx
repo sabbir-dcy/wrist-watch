05828c3a47e3ac63fb8650ce16b9add0d81bc4a0
sabbir-dcy <sabbirahmd.bracu@gmail.com>
2022-03-26 12:52:02 +0600

    random product 2

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,23 +4,54 @@ import style from "./Cart.module.css";
 
 const Cart = ({ products }) => {
   const [randomProduct, setRandomProduct] = useState("");
+  const [disable, setDisable] = useState(false);
+  const [display, setDisplay] = useState("1");
+  const [visibility, setVisibility] = useState("hidden");
+
   const randomChoice = () => {
-    const random = Math.floor(Math.random() * 4);
-    setRandomProduct(products[random].name)
+    // random number cant be more than the products have in cart
+    if (products.length < 2) {
+      alert("choose two products atleast");
+      return;
+    }
+    const random = Math.floor(Math.random() * products.length);
+    chooseOne(random)
   };
+
+  const chooseOne = (random) => {
+    setRandomProduct(products[random]);
+    setVisibility('visible')
+    setDisable(true);
+    setDisplay(".2");
+  }
+
   return (
     <div className={style.cart}>
       <h2 className={style.cart__title}>watch busket</h2>
-      {products.map((product) => (
-        <AddedProduct
-          name={product.name}
-          img={product.img}
-          key={Math.random()}
-        />
-        // used math.random incase multiple product get added to cart. because similar product has same id
-      ))}
-      <h2>{randomProduct}</h2>
-      <button onClick={randomChoice} className={style.random__btn}>
+      <div style={{ opacity: `${display}` }}>
+        {products.map((product) => (
+          <AddedProduct
+            name={product.name}
+            img={product.img}
+            key={Math.random()}
+          />
+          // used math.random incase multiple product get added to cart. because similar product has same id
+        ))}
+      </div>
+
+      <div
+        className={style.random__product}
+        style={{ visibility: `${visibility}` }}
+      >
+        <img src={randomProduct.img} alt="" />
+        <h2>{randomProduct.name}</h2>
+      </div>
+
+      <button
+        onClick={randomChoice}
+        disabled={disable}
+        className={style.random__btn}
+      >
         random choice
       </button>
     </div>
